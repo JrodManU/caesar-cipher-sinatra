899313c4d9c5edb03e7e319f7cb95cf0d37acf25
JrodManU <jrodmanu@yahoo.com>
2017-06-11 21:59:24 -0400

    finished project

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -3,5 +3,28 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 get '/' do
-  erb :game
+  new_string = ''
+  if params['original_string'] && params['shift_number']
+    new_string = cipher(params['original_string'], params['shift_number'].to_i)
+  end
+  erb :game, :locals => { :new_string => new_string }
+end
+
+def cipher(string, shift_number)
+	new_string = ''
+	string.each_char do |char|
+		if char.match(/[a-zA-Z]/)
+			shift_number.times do
+				if char.match(/z/)
+					char = 'a'
+				elsif char.match(/Z/)
+					char = 'A'
+				else
+					char.next!
+				end
+			end
+		end
+		new_string << char
+	end
+	return new_string
 end
